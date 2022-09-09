0df04fdd690f9c1f70bb28f934d406a62c8780a0
Carla Oliveira <54954245+carlasoliveira@users.noreply.github.com>
2022-09-09 05:26:00 -0300

    Curso SQL - Lista - Aula 5

---
+++ aula5-lista-curso.sql
@@ -0,0 +1,61 @@
+/*
+INSERT -Lista Exercício 
+Caso queira fazer na prática, execute o script do link: https://github.com/heliokamakawa/curso_bd/blob/master/05a-SELECT%20-%20script%20aula.sql
+*/
+
+-- 1) Escreva o comando para inserir um registro da tabela estado com todas as colunas. 
+INSERT INTO estado VALUES (NULL,'ACRE','AC','S','2021-05-01'); 
+/* 
+Como id é um campo com auto incremento, definimos NULL no comando.
+Como estamos inserindo todas as colunas não precisamos informar a descrição das colunas. 
+Porém, devemos informar os dados na sequência correta 
+**/ 
+
+-- 2) Escreva o comando para inserir um registro da tabela estado, definindo todos os dados, exceto a  chave primária que é auto incremento.  
+INSERT INTO estado (nome,sigla,ativo,data_cadastro) 
+VALUES ('ALAGOAS','AL','S','2021-05-02'); 
+-- Como não estamos informando todos os dados, é necessário especificar de quais colunas são 
+
+-- 3) Escreva o comando para inserir um registro da tabela estado, definindo somente os dados  necessários.  
+INSERT INTO estado (nome,sigla) VALUES ('AMAPÁ','AP'); 
+/** 
+id → não precisa informar porque é auto incremento  
+ativo e data_cadastro → não precisa informar porque possui o valor padrão  
+**/ 
+
+-- 4) Escreva o comando para inserir registros da tabela cidade das 3 formas apresentadas nos exercícios  anteriores.  
+-- O registro de uma cidade requer a referência do estado (cidade é dependente  do estado). Assim, é necessário, primeiro, fazer a inserção dos estados. 
+INSERT INTO cidade (id,nome,estado_id,ativo,data_cadastro) VALUES (NULL, 'ACRELANDIA',1,'S','2021-04-28'); 
+INSERT INTO cidade (nome,estado_id,ativo,data_cadastro) VALUES ('ASSIS  BRASIL',1,'S','2021-03-14'); 
+INSERT INTO cidade (nome,estado_id) VALUES ('BRASILEIA',1);
+
+/*
+AGORA É A SUA VEZ!!!! Para que você aprenda, é muito importante que não copie e cole,  digite os comandos e mentalize o que está fazendo na medida que digita cada comando. Uma dica  importante é que você digite primeiro no bloco de notas, com intuito de testar se realmente  consegue digitar os comandos sem a ajuda do IDE – depois teste os comandos. 
+5. Escreva o comando para inserir 3 registros da tabela estado com todas as colunas. 
+
+INSERT INTO cidade (id,nome,estado_id,ativo,data_cadastro) VALUES (DEFAULT, 'PARANAVAÍ',1,'S','2022-09-04'); 
+INSERT INTO cidade (id,nome,estado_id,ativo,data_cadastro) VALUES (DEFAULT, 'PARAISO DO NORTE',1,'S','2022-09-04'); 
+INSERT INTO cidade (id,nome,estado_id,ativo,data_cadastro) VALUES (DEFAULT, 'SAO CARLOS DO IVAI',1,'S','2022-09-04'); 
+
+6. Escreva o comando para inserir 2 registros da tabela estado, definindo todos os dados, exceto a chave  primária que é auto incremento. 
+
+INSERT INTO cidade (nome,estado_id,ativo,data_cadastro) VALUES ('ALTO PARANA',1,'S','2022-09-04'); 
+INSERT INTO cidade (nome,estado_id,ativo,data_cadastro) VALUES ('MARINGA',1,'S','2022-09-04'); 
+
+7. Escreva o comando para inserir 2 registros da tabela estado, definindo somente os dados necessários. 
+
+INSERT INTO cidade (nome,estado_id) VALUES ('ALTO PIQUIRI',1);
+INSERT INTO cidade (nome,estado_id) VALUES ('CURITIBA',1);
+
+8. Escreva o comando para inserir registros da tabela cidade das 3 formas apresentadas nos exercícios  anteriores.  
+
+INSERT INTO cidade (id,nome,estado_id,ativo,data_cadastro) VALUES (DEFAULT, 'UMUARAMA',1,'S','2022-09-04'); 
+INSERT INTO cidade (nome,estado_id,ativo,data_cadastro) VALUES ('CIANORTE',1,'S','2022-09-04'); 
+INSERT INTO cidade (nome,estado_id) VALUES ('LONDRINA',1);
+
+9. Faça a inserção de 2 registros de cliente.  
+
+INSERT INTO cliente (id, nome, cpf, email) VALUES(1, 'Carla', 12345678900, carla@gmail.com), (2, 'Tavares', 98765432100, tavares@gmail.com);
+
+10. DESAFIO!!! Tente fazer todas as inserções necessárias para que se tenha um item de caixa. Na medida  que esteja digitando o código, tente associar os dados inseridos com o contexto real.
+*/
