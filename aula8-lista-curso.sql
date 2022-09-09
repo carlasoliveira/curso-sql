5c7e365e2ae4420f501f9b52361e7c2e5a40d686
Carla Oliveira <54954245+carlasoliveira@users.noreply.github.com>
2022-09-09 06:07:03 -0300

    Curso SQL - Lista - Aula 8

---
+++ aula8-lista-curso.sql
@@ -0,0 +1,11 @@
+01 - O que é JOIN? Quando é necessário?
+  O JOIN é um comando do SQL cujo objetivo é unir colunas de tabelas distintas; portanto, é utilizado quando se deseja combinar, em uma mesma consulta, uma ou mais colunas que pertecem a diferentes tabelas em um banco de dados, tornando a consulta mais objetiva e precisa.
+  
+02 - Qual a sintaxe do JOIN? Maiúscula ou minúscula faz diferença? Existe algum padrão? Explique.
+  Tal como nas demais funções do SQL, não há diferença entre escrever o JOIN com letras maiúsculas ou minúsculas. No entanto, é louvável que seja adotado um padrão para escrita de scripts em SQL e, dessa forma, é comum que palavras reservadas, como o JOIN, sejam escritas sempre em letras maiúsculas.
+  
+04 - O que é primordial para que o resultado tenha sentido em consultas com JOIN? Explique.
+  É imprescindível que haja atenção na construção da sentença que forma a seleção, analisando as tabelas, colunas e condições definidas, visto que é necessária a sintaxe correta para que as consultas sejam devidamente realizadas.
+  
+05 - Existe mais de uma maneira de realizar o JOIN? Quantas? Qual é a mais eficiente?
+  O JOIN pode ser realizado de 5 maneiras: INNER JOIN, LEFT JOIN, RIGHT JOIN, FULL JOIN e CROSS JOIN. A eficiência dependerá da necessidade, que por sua vez depende do contexto em que o sistema se encontra e do que ele precisa em relação às consultas no banco de dados.
